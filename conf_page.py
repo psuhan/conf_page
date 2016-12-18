9d7449643d83f8b4772ee0ca7dd6ff43329c261c
pshan76 <pyungsu.han@gmail.com>
2016-12-18 15:42:40 +0000

    second commit

--- conf_page.py
+++ conf_page.py
@@ -16,14 +16,14 @@ class page(object):
 
 	__ROOT_TAG_HEAD__ = '<root xmlns:ac="confluence_macro">'
 	__ROOT_TAG_TAIL__ = '</root>'
-	__COOKIE_JAR_FILE__ = '.confluence_cookie_jar'
+	__PREVIOUS_SESSION_DATA__ = '.previous_session_data'
 
 	def __init__(self):
 		#self.headers = {'Content-Type': 'application/json'}
 		self.__logged = False
 		self.rest_server = ''
 		self.session=requests.session()
-		self.__COOKIE_JAR_FILE__ = os.environ['HOME'] + '/' + self.__COOKIE_JAR_FILE__
+		self.__PREVIOUS_SESSION_DATA__ = os.environ['HOME'] + '/' + self.__PREVIOUS_SESSION_DATA__
 		self.last_response_json = {}
 		self.initialize()
 
@@ -37,14 +37,15 @@ class page(object):
 		self.page_tree = None
 
 	def __del__(self):
-		self.__save_cookies()
+		self.save_session_id()
 
-	def __save_cookies(self):
+	def save_session_id(self):
 		if self.__logged:
-			with open(self.__COOKIE_JAR_FILE__, 'w') as f:
+			with open(self.__PREVIOUS_SESSION_DATA__, 'w') as f:
 				cookies = self.session.cookies.get_dict()
-				pickle.dump(cookies, f)
-			print 'session cookie saved to {}'.format(self.__COOKIE_JAR_FILE__)
+				session_data = {'JSESSIONID':cookies['JSESSIONID'], 'user_id':self.user_id}
+				pickle.dump(session_data, f)
+			print 'session data saved to {}'.format(self.__PREVIOUS_SESSION_DATA__)
 
 	def set_rest_server(self, server):
 		self.rest_server = server
@@ -55,26 +56,34 @@ class page(object):
 	def __remove_root_tag(self, content):
 		return content[content.find(self.__ROOT_TAG_HEAD__) + len(self.__ROOT_TAG_HEAD__):content.find(self.__ROOT_TAG_TAIL__)]
 
-	def __login(self):
-		if os.path.isfile(self.__COOKIE_JAR_FILE__):
-			print 'cookie jar file found, continue previous session'
-			with open(self.__COOKIE_JAR_FILE__, 'r') as f:
-				cookies = pickle.load(f)
-			self.session.cookies.update(cookies)
+	def do_login(self):
+		if os.path.isfile(self.__PREVIOUS_SESSION_DATA__):
+			print 'trying to continue previous session'
+			with open(self.__PREVIOUS_SESSION_DATA__, 'r') as f:
+				session_data = pickle.load(f)
+			self.session.cookies.update({'JSESSIONID':session_data['JSESSIONID']})
 			ret = self.session.head(self.rest_server + '?')
+			if ret.status_code == 200 and ret.headers['X-AUSERNAME'] == session_data['user_id']:
+				print 'continuing previous session'
+				self.__logged = True
+				self.user_id = session_data['user_id']
+				return True
+			else:
+				print 'login failed'
+				return False
 		else:
-			user = raw_input('Confluence user ID: ')
-			ret = self.session.head(self.rest_server + '?', auth=(user, getpass.getpass('password: ')))
-		if ret.status_code == 200:
-			print 'login success'
-			self.__logged = True
-			return True
-		else:
-			print 'login failed'
-			return False
+			self.user_id = raw_input('Confluence user ID: ')
+			ret = self.session.head(self.rest_server + '?', auth=(self.user_id, getpass.getpass('password: ')))
+			if ret.status_code == 200 and ret.headers['X-AUSERNAME'] == self.user_id:
+				print 'logged in'
+				self.__logged = True
+				return True
+			else:
+				print 'login failed'
+				return False
 
 	def rest_api_get(self, url, args):
-		if self.__logged or self.__login():
+		if self.__logged or self.do_login():
 			exts = []
 			for (key, val) in args.items():
 				if type(val) == list:
@@ -130,6 +139,20 @@ class page(object):
 				return False
 			else:
 				return self.last_response_json['results'][0]['id']
+
+	def delete_page(self, title, space):
+		if self.__logged or self.do_login():
+			pid = self.get_page_id_from_title(title, space)
+			if not pid == False:
+				ret = self.session.delete(self.rest_server + '/{}'.format(pid))
+				if ret.status_code == 204:
+					print 'page deleted'
+					return True
+				else:
+					print 'cannot delete page'
+			else:
+				#print 'no page found titled: {} in space: {}'.format(title, space)
+				return False
 	
 	def upload_page_to_web(self, title, space, parent):
 		if self.synced:
@@ -168,12 +191,15 @@ logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 #print p.get_page_id_from_title('test_document', 'SICIPT')
 p2 = page()
 p2.set_rest_server('http://confluence.augkorea.org/rest/api/content')
-p2.get_page_from_server('playground', 'PG')
-
-p2.create_new_page_offline()
-p = ET.SubElement(p2.page_tree, 'p')
-p.text = 'new page'
-p2.upload_page_to_web('test_document5', 'PG', 'playground')
+p2.delete_page('child1', 'PG')
+p2.delete_page('child2', 'PG')
+#p2.delete_page('test_document5', 'PG')
+#p2.delete_page('test_document6', 'PG')
+#p2.get_page_from_server('playground', 'PG')
+#p2.create_new_page_offline()
+#p = ET.SubElement(p2.page_tree, 'p')
+#p.text = 'new page'
+#p2.upload_page_to_web('test_document6', 'PG', 'playground')
 
 
 
