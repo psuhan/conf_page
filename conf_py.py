311295b9960454b0166b067b730bf7ba1a6cb3fe
Pyungsu Han <ps.han@lge.com>
2016-12-20 16:27:41 +0900

    file attachment

--- conf_py.py
+++ conf_py.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import lxml.etree as ET
+#import lxml.etree as ET
 import requests
 import requests_toolbelt
 import pickle
@@ -10,7 +10,7 @@ import json
 import logging
 import base64
 
-class page(object):
+class conf_rest_api(object):
 	"""
 	create, modify and upload pages to Atlassian Confluence
 	itended to run on Linux
@@ -23,7 +23,7 @@ class page(object):
 	def __init__(self):
 		#self.headers = {'Content-Type': 'application/json'}
 		self.logged = False
-		self.rest_server = ''
+		self.server = ''
 		self.session=requests.session()
 		self.__PREVIOUS_SESSION_FOLDER__ = os.environ['HOME'] + '/' + self.__PREVIOUS_SESSION_FOLDER__
 		self.last_response_json = {}
@@ -32,11 +32,10 @@ class page(object):
 	def initialize(self):
 		self.title = ''
 		self.space = ''
-		self.synced = False
 		self.page_string = ''
 		self.page_version = 0
 		self.page_id = 0
-		self.page_tree = None
+		#self.page_tree = None
 
 	def __del__(self):
 		self.save_sessions()
@@ -51,15 +50,15 @@ class page(object):
 				if not os.path.isdir(self.__PREVIOUS_SESSION_FOLDER__):
 					print 'cannot create session data folder: {}'.format(self.__PREVIOUS_SESSION_FOLDER__)
 					return False
-			site_file_name = self.__PREVIOUS_SESSION_FOLDER__ + '/{}'.format(base64.b64encode(self.rest_server))
+			site_file_name = self.__PREVIOUS_SESSION_FOLDER__ + '/{}'.format(base64.b64encode(self.server))
 			with open(site_file_name, 'w') as f:
 				cookies = self.session.cookies.get_dict()
 				session_data = {'user_id':self.user_id, 'cookies':cookies}
 				pickle.dump(session_data, f)
 				logging.debug('session data saved to {}'.format(site_file_name))
 
-	def set_rest_server(self, server):
-		self.rest_server = server
+	def set_server(self, server):
+		self.server = server
 
 	def define_dummy_ns(self, content):
 		return self.__ROOT_TAG_HEAD__ + content + self.__ROOT_TAG_TAIL__
@@ -68,19 +67,18 @@ class page(object):
 		return content[content.find(self.__ROOT_TAG_HEAD__) + len(self.__ROOT_TAG_HEAD__):content.find(self.__ROOT_TAG_TAIL__)]
 
 	def do_login(self):
-		if not self.logged and not self.rest_server == '':
+		if not self.logged and not self.server == '':
 			print 'trying to continue previous session'
 		else:
 			logging.debug('alread logged in or server is not specified')
 			return False
-		site_file_name = self.__PREVIOUS_SESSION_FOLDER__ + '/{}'.format(base64.b64encode(self.rest_server))
+		site_file_name = self.__PREVIOUS_SESSION_FOLDER__ + '/{}'.format(base64.b64encode(self.server))
 		if os.path.isfile(site_file_name):
 			with open(site_file_name, 'r') as f:
 				session_data = pickle.load(f)
 			self.session.cookies.update(session_data['cookies'])
 			self.user_id = session_data['user_id']
-			ret = self.session.head(self.rest_server + '?')
-			#if ret.status_code == 200 and ret.headers['X-AUSERNAME'] == self.user_id:
+			ret = self.session.head(self.server + '?')
 			if ret.status_code == 200 and 'X-AUSERNAME' in ret.headers and ret.headers['X-AUSERNAME'] == self.user_id:
 				print 'continuing previous session'
 				self.logged = True
@@ -89,10 +87,9 @@ class page(object):
 				print 'login failed'
 				return False
 		else:
-			print 'no saved sessions for {}'.format(self.rest_server)
+			print 'no saved sessions for {}'.format(self.server)
 			self.user_id = raw_input('Confluence user ID: ')
-			ret = self.session.head(self.rest_server + '?', auth=(self.user_id, getpass.getpass('password: ')))
-			#if ret.status_code == 200 and ret.headers['X-AUSERNAME'] == self.user_id:
+			ret = self.session.head(self.server + '?', auth=(self.user_id, getpass.getpass('password: ')))
 			if ret.status_code == 200 and 'X-AUSERNAME' in ret.headers and ret.headers['X-AUSERNAME'] == self.user_id:
 				print 'logged in'
 				self.logged = True
@@ -103,7 +100,7 @@ class page(object):
 				print 'login failed'
 				return False
 
-	def rest_api_get(self, url, args):
+	def rest_get(self, url, args):
 		if self.logged or self.do_login():
 			exts = []
 			for (key, val) in args.items():
@@ -113,7 +110,7 @@ class page(object):
 					exts.append(key + '=' + str(val))
 			exts = '?' + '&'.join(exts)
 			logging.debug('extension: ' + exts)
-			ret = self.session.get(self.rest_server + exts)
+			ret = self.session.get(self.server + exts)
 			if ret.status_code == 200:
 				self.last_response_json = json.loads(ret.text)
 				return True
@@ -123,8 +120,30 @@ class page(object):
 		else:
 			return False
 			
-	def get_page_from_server(self, title, space):
-		if self.rest_api_get(self.rest_server, {'title':title, 'spaceKey':space, 'expand':['body.storage', 'version']}):
+	def get_page_id(self, title, space):
+		if self.rest_get(self.server, {'title':title, 'spaceKey':space}):
+			if self.last_response_json['size'] < 1:
+				print 'no page found titled: {} in space: {}'.format(title, space)
+				return False
+			elif self.last_response_json['size'] > 1:
+				print 'more than 2 pages returned'
+				return False
+			else:
+				return self.last_response_json['results'][0]['id']
+
+	def get_page_version(self, title, space):
+		if self.rest_get(self.server, {'title':title, 'spaceKey':space, 'expand':'version'}):
+			if self.last_response_json['size'] < 1:
+				print 'no page found titled: {} in space: {}'.format(title, space)
+				return False
+			elif self.last_response_json['size'] > 1:
+				print 'more than 2 pages returned'
+				return False
+			else:
+				return self.last_response_json['results'][0]['version']['number']
+
+	def get_page(self, title, space):
+		if self.rest_get(self.server, {'title':title, 'spaceKey':space, 'expand':['body.storage', 'version']}):
 			if self.last_response_json['size'] < 1:
 				print 'no page found titled: {} in space: {}'.format(title, space)
 				return False
@@ -135,70 +154,119 @@ class page(object):
 				self.page_string = self.last_response_json['results'][0]['body']['storage']['value']
 				self.version = self.last_response_json['results'][0]['version']['number']
 				self.page_id = self.last_response_json['results'][0]['id']
-				self.page_tree = ET.fromstring(self.define_dummy_ns(self.page_string))
+				#self.page_tree = ET.fromstring(self.define_dummy_ns(self.page_string))
 				self.title = title
 				self.space = space
-				self.synced = True
+				print 'page downloaded'
 				return True
 		else:
 			print 'cannot get respond from server'
 			return False
 
-	def create_new_page_offline(self):
-		self.initialize()
-		self.synced = False
-		self.page_tree = ET.fromstring(self.__ROOT_TAG_HEAD__ + self.__ROOT_TAG_TAIL__)
-
-	def get_page_id_from_title(self, title, space):
-		if self.rest_api_get(self.rest_server, {'title':title, 'spaceKey':space}):
-			if self.last_response_json['size'] < 1:
-				print 'no page found titled: {} in space: {}'.format(title, space)
-				return False
-			elif self.last_response_json['size'] > 1:
-				print 'more than 2 pages returned'
-				return False
-			else:
-				return self.last_response_json['results'][0]['id']
+	##def create_new_page_offline(self):
+	##	self.initialize()
+	##	self.page_tree = ET.fromstring(self.__ROOT_TAG_HEAD__ + self.__ROOT_TAG_TAIL__)
 
 	def delete_page(self, title, space):
 		if self.logged or self.do_login():
-			pid = self.get_page_id_from_title(title, space)
+			pid = self.get_page_id(title, space)
 			if not pid == False:
-				ret = self.session.delete(self.rest_server + '/{}'.format(pid))
+				ret = self.session.delete(self.server + '/{}'.format(pid))
 				if ret.status_code == 204:
 					print 'page deleted'
 					return True
 				else:
 					print 'cannot delete page'
 			else:
-				#print 'no page found titled: {} in space: {}'.format(title, space)
 				return False
 	
-	def upload_page_to_web(self, title, space, parent):
-		if self.synced:
-			print 'page already synced to web'
+	def upload_page(self, title, space, parent, page_string = ''):
+		pid = self.get_page_id(parent, space)
+		if pid == False:
+			print 'parent page: {} does not exist'.format(parent)
 			return False
 		else:
-			pid = self.get_page_id_from_title(parent, space)
-			if pid == None:
-				print 'parent page: {} does not exist'.format(parent)
+			#self.page_string = self.remove_root_tag(ET.tostring(self.page_tree))
+			if page_string == '': page_string = self.page_string
+			data = {'title':		title,
+			        'space':		{'key': space},
+			        'ancestors':	[{'id':pid}],
+			        'type':			'page',
+			        'body':			{'storage':{'value': page_string, 'representation':'storage'}}
+			}
+			res = self.session.post(self.server, json=data)
+			if res.status_code == 200:
+				#self.title = title
+				#self.space = space
+				print 'page uploaded'
+				return True
 			else:
-				self.page_string = self.remove_root_tag(ET.tostring(self.page_tree))
-				#print self.page_string
-				data = {'title':		title,
-				        'space':		{'key': space},
-				        'ancestors':	[{'id':pid}],
-				        'type':			'page',
-				        'body':			{'storage':{'value': self.page_string, 'representation':'storage'}}
-				}
-				res = self.session.post(self.rest_server, json=data)
-				if res.status_code == 200:
-					self.synced = True
-					self.title = title
-					self.space = space
-					#Self.page_version = 0
-					#Self.page_id = 0
-				else:
-					print 'HTTP status code: {}'.format(res.status_code)
-					print 'page upload failed'
+				print 'HTTP status code: {}'.format(res.status_code)
+				print 'page upload failed'
+				return False
+
+	def update_page(self, title, space, page_string = ''):
+		pid = self.get_page_id(title, space)
+		if pid == False:
+			print 'parent page: {} does not exist'.format(parent)
+			return False
+		ver = self.get_page_version(title, space)
+		if ver == False:
+			print 'cannot get page version'
+			return False
+		else:
+			if page_string == '': page_string = self.page_string
+			ver = ver + 1
+			data = {'title':		title,
+			        'type':			'page',
+			        'body':			{'storage':{'value': page_string, 'representation':'storage'}},
+			        'version':		{'number': ver}
+			}
+			res = self.session.put(self.server + '/{}'.format(pid), json=data)
+			if res.status_code == 200:
+				#self.title = title
+				#self.space = space
+				#self.version = ver
+				#self.page_id = pid
+				print 'page updated'
+				return True
+			else:
+				print 'HTTP status code: {}'.format(res.status_code)
+				print 'page update failed'
+				return False
+		
+	def attach_file(self, title, space, file, comment=''):
+		""" attach file to an existing collab page """
+		pid = self.get_page_id(title, space)
+		base_name = os.path.basename(file)
+		if pid == False:
+			print 'page: {} does not exist'.format(parent)
+			return False
+		else:
+			### see if attachment alread exists
+			url = self.server + '/{}/child/attachment?'.format(pid)
+			res = self.session.get(url)
+			attachments = res.json()['results']
+			att_id = None
+			for at in attachments:
+				if unicode(at['title']) == unicode(base_name):
+					att_id = at['id']
+			if att_id == None: ## new file
+				files = [('file', (base_name, open(file, 'rb'), 'application/octet-stream'))]
+				url = self.server + '/{}/child/attachment'.format(pid)
+			else:	## update
+				files = [('file', (base_name, open(file, 'rb'), 'application/octet-stream')), ('minorEdit', 'true')]
+				url = self.server + '/{}/child/attachment/{}/data'.format(pid, att_id)
+				print 'file already exist, trying to updates'
+			if not comment == '':
+				files.append(('comment', comment))
+			headers = {'X-Atlassian-Token': 'nocheck'}
+			res = self.session.post(url, headers=headers, files=files)
+			if res.status_code == 200:
+				print 'file attached'
+				return True
+			else:
+				print 'cannot attach/update file'
+				#print dump.dump_all(res).decode('utf-8')
+				return False
 
